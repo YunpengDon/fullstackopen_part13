983837eeadab9d4848f9b37377c7db26e909283f
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-02-27 22:46:42 +0800

    complete exercise 13.2

---
+++ commands.sql
@@ -0,0 +1,10 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer DEFAULT 0
+);
+
+insert into blogs (author, url, title) values ('test author1', 'url 1' , 'title 1');
+insert into blogs (author, url, title) values ('test author2', 'url 2' , 'title 2');
