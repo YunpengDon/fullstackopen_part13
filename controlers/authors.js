41042a7e17388b22e3912bdc6c92f57bbc6febf9
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-03-03 23:47:32 +0800

    complete exercise 13.16

--- controlers/authors.js
+++ controlers/authors.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 const { Blog } = require("../models");
+const sequelize = require("sequelize");
 
 router.get("/", async (req, res) => {
   const authors = await Blog.findAll({
-    group: 'author'
+    group: "author",
+    attributes: [
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("title")), "articles"],
+      [sequelize.fn("SUM", sequelize.col("likes")), "likes"],
+    ],
+    order: [["likes", "DESC"]],
   });
   res.json(authors);
 });
