2fac4b5d356de1fb8038726e4b509db4e860e769
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-03-03 14:12:51 +0800

    complete exercise 13.10

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require("express-async-errors");
 const app = express();
 const blogRouter = require("./controlers/blogs");
 const userRouter = require("./controlers/users");
+const loginRouter = require("./controlers/login");
 const middleware = require("./util/middleware");
 const { PORT } = require("./util/config");
 
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
