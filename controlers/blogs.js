e11f9f1d886db35665f9151eff56793a5f36e574
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-03-06 01:04:48 +0800

    complete exercise 13.22

--- controlers/blogs.js
+++ controlers/blogs.js
@@ -1,22 +1,7 @@
-const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 const { Op } = require("sequelize");
 const { Blog, User } = require("../models");
-const { SECRET } = require("../util/config");
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
+const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", tokenExtractor, async (req, res) => {
   const blogs = await Blog.findAll({
--- controlers/readingList.js
+++ controlers/readingList.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Blog, ReadingList } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", async (req, res) => {
   const list = await ReadingList.findAll({
@@ -10,11 +11,30 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   if (req.body) {
-    const user = await User.create({
+    const list = await ReadingList.create({
       ...req.body,
     });
-    res.json(user);
+    res.json(list);
   }
+});
+
+router.put("/:id", tokenExtractor, async (req, res, next) => {
+  const userId = req.decodedToken.id;
+  const list = await ReadingList.findByPk(req.params.id);
+  if (userId !== list.userId) {
+    const error = new Error(
+      "Only the user who own the list can mark it as read"
+    );
+    error.name = "ValidationError";
+    return next(error);
+  }
+  if (req.body.read !== undefined) {
+    const result = await list.update({
+      read: req.body.read,
+    });
+    return res.json(result);
+  }
+  throw new SyntaxError("Attribute 'read' is required");
 });
 
 module.exports = router;
